eb4f1e92b859d13d9256610ea5b189a672642630
Andy D. Nguyen <10527554+andyngdz@users.noreply.github.com>
2025-08-05 07:39:52 +0700

    feat: increase gap in SetupLayoutContent for improved layout spacing

--- src/components/layout/presentations/SetupLayoutContent.tsx
+++ src/components/layout/presentations/SetupLayoutContent.tsx
@@ -10,7 +10,7 @@ export const SetupLayoutContent: FC<
 > = ({ title, description, children }) => {
   return (
     <div className="max-w-4xl flex flex-1 items-center justify-center">
-      <div className="mx-auto flex flex-col gap-8">
+      <div className="mx-auto flex flex-col gap-20">
         <div className="flex flex-col gap-2 items-center justify-center">
           <h1 className="text-4xl font-bold text-base-content">{title}</h1>
           <span className="text-base-content/70">{description}</span>
