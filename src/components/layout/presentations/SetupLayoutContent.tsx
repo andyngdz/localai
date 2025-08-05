875a77dc9e52ff6bdfc308d90a947728b95901e2
Andy D. Nguyen <10527554+andyngdz@users.noreply.github.com>
2025-08-05 07:57:09 +0700

    feat: add experimental view transition and adjust layout spacing in SetupLayoutContent

--- src/components/layout/presentations/SetupLayoutContent.tsx
+++ src/components/layout/presentations/SetupLayoutContent.tsx
@@ -10,12 +10,12 @@ export const SetupLayoutContent: FC<
 > = ({ title, description, children }) => {
   return (
     <div className="max-w-4xl flex flex-1 items-center justify-center">
-      <div className="mx-auto flex flex-col gap-20">
+      <div className="mx-auto flex flex-col gap-12">
         <div className="flex flex-col gap-2 items-center justify-center">
           <h1 className="text-4xl font-bold text-base-content">{title}</h1>
           <span className="text-base-content/70">{description}</span>
         </div>
-        <div className="flex justify-center">
+        <div className="flex flex-1 justify-center">
           <div className="container">{children}</div>
         </div>
       </div>
