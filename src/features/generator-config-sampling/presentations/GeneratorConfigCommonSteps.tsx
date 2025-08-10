96fb87e5f8ed1135370de9f64d5337d46b5cbe98
Andy D. Nguyen <10527554+andyngdz@users.noreply.github.com>
2025-08-10 22:09:57 +0700

    feat: enhance GeneratorConfigCommonSteps to utilize useFormContext for step selection

--- src/features/generator-config-sampling/presentations/GeneratorConfigCommonSteps.tsx
+++ src/features/generator-config-sampling/presentations/GeneratorConfigCommonSteps.tsx
@@ -1,11 +1,23 @@
+import { GeneratorConfigFormValues } from "@/features/generator-configs/types/generator-config";
 import { Button } from "@heroui/react";
+import { useFormContext } from "react-hook-form";
 import { COMMON_STEPS } from "../constants";
 
 export const GeneratorConfigCommonSteps = () => {
+  const { setValue } = useFormContext<GeneratorConfigFormValues>();
+
   return (
     <div className="flex">
       {COMMON_STEPS.map((step) => (
-        <Button key={step} variant="light" className="text-foreground-500" isIconOnly>
+        <Button
+          key={step}
+          variant="light"
+          className="text-foreground-500"
+          onPress={() => {
+            setValue("steps", step);
+          }}
+          isIconOnly
+        >
           {step}
         </Button>
       ))}
