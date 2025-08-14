8cce5f3b18e9703e62da3a969351b944d82aa42c
Andy D. Nguyen <10527554+andyngdz@users.noreply.github.com>
2025-08-14 07:42:14 +0700

    fix: correct variable name typos in GeneratorConfigStyleSection component

--- src/features/generator-config-styles/presentations/GeneratorConfigStyleSection.tsx
+++ src/features/generator-config-styles/presentations/GeneratorConfigStyleSection.tsx
@@ -9,14 +9,14 @@ interface GeneratorConfigStyleSectionProps {
 export const GeneratorConfigStyleSection: FC<GeneratorConfigStyleSectionProps> = ({
   styleSections,
 }) => {
-  return styleSections.map((styleSeection) => {
+  return styleSections.map((styleSection) => {
     return (
-      <div key={styleSeection.id} className="flex flex-col gap-2">
+      <div key={styleSection.id} className="flex flex-col gap-2">
         <span className="text-lg font-medium capitalize bg-foreground-100 p-2">
-          {styleSeection.id}
+          {styleSection.id}
         </span>
         <div className="flex flex-wrap gap-2 p-2">
-          {styleSeection.styles.map((styleItem) => {
+          {styleSection.styles.map((styleItem) => {
             return <GeneratorConfigStyleItem key={styleItem.id} styleItem={styleItem} />;
           })}
         </div>
