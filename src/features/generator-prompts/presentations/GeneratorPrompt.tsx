b0186bf34ba7daac53e0cacf793f55eba7bf0622
Andy D. Nguyen <10527554+andyngdz@users.noreply.github.com>
2025-08-14 08:24:19 +0700

    fix: add maxLength attribute to Textarea components in GeneratorPrompt

--- src/features/generator-prompts/presentations/GeneratorPrompt.tsx
+++ src/features/generator-prompts/presentations/GeneratorPrompt.tsx
@@ -8,12 +8,13 @@ export const GeneratorPrompt = () => {
   return (
     <div className="p-4">
       <div className="flex gap-4">
-        <Textarea className="font-mono" label="Prompt" {...register("prompt")}></Textarea>
+        <Textarea className="font-mono" label="Prompt" maxLength={1000} {...register("prompt")} />
         <Textarea
           className="font-mono"
           label="Negative prompt"
+          maxLength={1000}
           {...register("negative_prompt")}
-        ></Textarea>
+        />
       </div>
     </div>
   );
