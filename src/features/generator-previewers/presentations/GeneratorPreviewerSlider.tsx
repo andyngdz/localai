4c17301bc6e310e2265da7b4617a3124aaa731ec
Andy D. Nguyen <10527554+andyngdz@users.noreply.github.com>
2025-09-03 09:16:55 +0700

    refactor: simplify image slider with 2 slides per view and reduced styling

--- src/features/generator-previewers/presentations/GeneratorPreviewerSlider.tsx
+++ src/features/generator-previewers/presentations/GeneratorPreviewerSlider.tsx
@@ -1,7 +1,6 @@
-import clsx from 'clsx'
 import { useMemo } from 'react'
+import { Keyboard, Mousewheel, Navigation, Pagination } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules'
 import { useGeneratorPreviewer } from '../states'
 import { GeneratorPreviewerItem } from './GeneratorPreviewerItem'
 
@@ -15,10 +14,8 @@ export const GeneratorPreviewerSlider = () => {
 
   const ImageSlides = useMemo(() => {
     return imageStepEnds.map((imageStepEnd) => (
-      <SwiperSlide key={imageStepEnd.index} className="flex justify-center items-center">
-        <div className="w-full max-w-2xl">
-          <GeneratorPreviewerItem imageStepEnd={imageStepEnd} />
-        </div>
+      <SwiperSlide key={imageStepEnd.index}>
+        <GeneratorPreviewerItem imageStepEnd={imageStepEnd} />
       </SwiperSlide>
     ))
   }, [imageStepEnds])
@@ -32,33 +29,20 @@ export const GeneratorPreviewerSlider = () => {
   }
 
   return (
-    <div className={clsx('w-full h-full p-4')}>
+    <div className="p-4">
       <Swiper
         modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-        spaceBetween={30}
-        slidesPerView={1}
-        navigation
+        spaceBetween={16}
+        slidesPerView={2}
         pagination={{
           clickable: true,
           dynamicBullets: true
-        }}
-        mousewheel={{
-          forceToAxis: true,
-          sensitivity: 0.1
         }}
         keyboard={{
           enabled: true,
           onlyInViewport: true
         }}
-        loop={imageStepEnds.length > 1}
-        centeredSlides
-        className={clsx(
-          'h-full w-full',
-          '[&_.swiper-button-next]:text-white [&_.swiper-button-prev]:text-white',
-          '[&_.swiper-button-next]:scale-75 [&_.swiper-button-prev]:scale-75',
-          '[&_.swiper-pagination-bullet]:bg-white [&_.swiper-pagination-bullet]:opacity-50',
-          '[&_.swiper-pagination-bullet-active]:opacity-100'
-        )}
+        navigation
       >
         {ImageSlides}
       </Swiper>
